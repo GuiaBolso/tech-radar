a00ff867d51cefbf41ea350d3fa9b1f3d2ec73e7
Octavio Turra <octavio@guiabolso.com.br>
2021-06-10 17:27:10 -0300

    Versão 2.0
    Colocando cores, logos, fonte e links

--- main.js
+++ main.js
@@ -1,74 +1,78 @@
 const QUADRANT = {
-    framework: 2,
-    language: 0,
-    tools: 3,
-    infra: 1,
+  framework: 2,
+  language: 0,
+  tools: 3,
+  infra: 1
 };
 
 const RING = {
-    hold: 3,
-    assess: 2,
-    trial: 1,
-    adopt: 0
+  hold: 3,
+  assess: 2,
+  trial: 1,
+  adopt: 0
 };
 
 async function loadData() {
-    const data = await fetch("techs.csv");
+  const data = await fetch("techs.csv");
 
-    if(data.status >= 200 && data.status < 400) {
-        return data.text();
-    }
+  if (data.status >= 200 && data.status < 400) {
+    return data.text();
+  }
 
-    throw new Error("Cannot load data");
+  throw new Error("Cannot load data");
 }
 
 async function prepareData(text) {
-    const lines = text.split(/\r?\n/gi);
-    const lineFields = lines.map(l => l.split(/;/gi));
-    return lineFields.slice(1);
+  const lines = text.split(/\r?\n/gi);
+  const lineFields = lines.map((l) => l.split(/;/gi));
+  return lineFields.slice(1);
 }
 
 async function generateEntries(data) {
-    return data
-    .filter(d => !!d[0])
-    .reduce((acc, curr) => (
-        [...acc, {
-            label:  `${curr[0]}`,
-            quadrant: QUADRANT[curr[1]],
-            ring: RING[curr[2]],
-            active: true,
-            moved: curr[4]
-        }]
-    ),[])
+  return data
+    .filter((d) => !!d[0])
+    .reduce(
+      (acc, curr) => [
+        ...acc,
+        {
+          label: `${curr[0]}`,
+          quadrant: QUADRANT[curr[1]],
+          ring: RING[curr[2]],
+          active: true,
+          moved: curr[4]
+        }
+      ],
+      []
+    );
 }
 
 function renderChart(entries) {
-    radar_visualization({
-        svg_id: "radar",
-        width: 1450,
-        height: 1000,
-        colors: {
-          background: "#fff",
-          grid: "#bbb",
-          inactive: "#ddd"
-        },
-        title: "Guiabolso Tech Radar",
-        quadrants: [
-          { name: "Linguagens" },
-          { name: "Infraestrutura" },
-          { name: "Frameworks e Bibliotecas" },
-          { name: "Técnicas e Ferramentas" }
-        ],
-        rings: [
-          { name: "ADOPT", color: "#93c47d" },
-          { name: "TRIAL", color: "#93d2c2" },
-          { name: "ASSESS", color: "#fbdb84" },
-          { name: "HOLD", color: "#efafa9" }
-        ],
-        print_layout: true,
-        // zoomed_quadrant: 0,
-        //ENTRIES
-        entries /* [
+  radar_visualization({
+    svg_id: "radar",
+    width: 1400,
+    height: 1000,
+    colors: {
+      background: "#fff",
+      grid: "#bbb",
+      inactive: "#ddd"
+    },
+    title: "Backend Tech Radar",
+    quadrants: [
+      { name: "Linguagens" },
+      { name: "Infraestrutura" },
+      { name: "Frameworks e Bibliotecas" },
+      { name: "Técnicas e Ferramentas" }
+    ],
+    rings: [
+      { name: "ADOPT", color: "#93c47d" },
+      { name: "TRIAL", color: "#93d2c2" },
+      { name: "ASSESS", color: "#fbdb84" },
+      { name: "HOLD", color: "#efafa9" }
+    ],
+    print_layout: true,
+    // zoomed_quadrant: 0,
+    //ENTRIES
+    entries /* [
             {
               label: "Kotlin",
               quadrant: 0,
@@ -98,11 +102,8 @@ function renderChart(entries) {
               moved: 0
             }
         ]*/
-        //ENTRIES
-      });
+    //ENTRIES
+  });
 }
 
-loadData()
-    .then(prepareData)
-    .then(generateEntries)
-    .then(renderChart);
+loadData().then(prepareData).then(generateEntries).then(renderChart);
